176d4cc28628d53497ab140ff2dfb50a709a742a
Nikita <98541680+kolofas@users.noreply.github.com>
2024-03-18 15:05:24 +0500

    Update db.py

--- db.py
+++ db.py
@@ -9,13 +9,13 @@ from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, joinedload
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '/home/nikita/PycharmProjects/pythonProject1/icq_bot/table.py')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '/path/to/your/file')))
 
 from table import User, Base, SingleReminder, CronModel
 from datetime import datetime
 from typing import Optional
 
-DATABASE_URL = 'postgresql+asyncpg://kolofas:okoko@localhost/icqdatabase'
+DATABASE_URL = 'DATABASE_URL'
 
 async_engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 sync_engine = create_engine(DATABASE_URL, echo=True)
@@ -31,7 +31,6 @@ async def connect_db():
             await conn.run_sync(Base.metadata.create_all)
         return async_session()
     except Exception as e:
-        print(f"Error in connect_db: {e}")
         return None
 
 
@@ -60,13 +59,11 @@ async def create_user(chat_id, user_id, username, session):
 
 
 async def get_user_by_chat_id(chat_id, user_id, session) -> Optional[User]:
-    print(f"Поиск пользователя с chat_id: {user_id}, {chat_id}")
     try:
         stmt = select(User).options(joinedload(User.reminders), joinedload(User.crons)).where(User.user_id == user_id)
-        print(f'stmt - {stmt}')
         result = await session.execute(stmt)
         user = result.scalar()
-        print(f"Получен пользователь с chat_id и user_id: {chat_id} {user_id}")
+        
         return user
     except exc.SQLAlchemyError as e:
         print(f"Ошибка в get_user_by_chat_id: {e}")
@@ -101,7 +98,6 @@ async def remove_reminder(chat_id, user_id, reminder_id, session: AsyncSession):
                 return True
 
     except SQLAlchemyError as e:
-        print(f"Ошбика в remove_reminder: {e}")
         await session.rollback()
 
     return False
@@ -109,12 +105,12 @@ async def remove_reminder(chat_id, user_id, reminder_id, session: AsyncSession):
 
 async def create_cron(user_id, chat_id, cron_expression, cron_type, user_message, session: AsyncSession):
     try:
-        print(f"User ID: {user_id}, Cron Expression: {cron_expression}")
         cron = CronModel(user_id=user_id, chat_id=chat_id, cron_expression=cron_expression, cron_type=cron_type,
                          user_message=user_message)
         session.add(cron)
         await session.commit()
         return True
+    
     except Exception as e:
         print(f"Error adding cron: {str(e)}")
         await session.rollback()
@@ -124,7 +120,6 @@ async def create_cron(user_id, chat_id, cron_expression, cron_type, user_message
 async def remove_cron(chat_id, user_id, cron_id, session):
     try:
         user = await get_user_by_chat_id(chat_id, user_id, session)
-        print(f"user - {user}")
 
         if user:
             stmt = select(CronModel).filter_by(id=cron_id, user=user)
